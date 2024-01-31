d1b73846b871ce0cdc46368a964e0cc769562a6d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-31 21:26:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Nav from "./files/Nav"
+
 function App() {
 
   return (
     <>
       <div>
-        <h1 className="text-blue-600">
-          Tailwind CSS
-        </h1>
+        <Nav />
       </div>
     </>
   )
